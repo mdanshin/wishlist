02ac11bab691e9469c20dcfc0835471e0bacd5ea
Mikhail Danshin <mdanshin@gmail.com>
2025-10-31 07:45:36 +0000

    Update Firebase configuration with correct API key and storage bucket

--- src/firebase.js
+++ src/firebase.js
@@ -4,11 +4,13 @@ import { getAuth, GoogleAuthProvider } from 'https://www.gstatic.com/firebasejs/
 import { getFirestore } from 'https://www.gstatic.com/firebasejs/9.22.2/firebase-firestore.js';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBWKwug69lRiWKZQ7vhMzTFLoxwFzDZhQA",
+  apiKey: "AIzaSyAGLAP1aQh9ZCTIYRz3y_wrPldnLEHssNQ",
   authDomain: "wishlist-962cf.firebaseapp.com",
   projectId: "wishlist-962cf",
-  storageBucket: "wishlist-962cf.appspot.com",
-  messagingSenderId: "665872423891"
+  storageBucket: "wishlist-962cf.firebasestorage.app",
+  messagingSenderId: "665872423891",
+  appId: "1:665872423891:web:43ebec3f6f5d6880472d26",
+  measurementId: "G-MC1CW8WCPP"
 };
 
 const app = initializeApp(firebaseConfig);
